f9d6eae15a1b3c514420a43b84dd15dd8c3cfee1
Haseeb Althaf <haseebalthaf1@outlook.com>
2024-01-10 13:17:18 +0530

    Final changes were made.

--- src/components/modalTabs.jsx
+++ src/components/modalTabs.jsx
@@ -36,7 +36,6 @@ function ModalTabs({ selectedProperty }) {
               title="Google Map"
               width="100%"
               height="400"
-              frameBorder="0"
               style={{ border: 0 }}
               src={selectedProperty.url}
               allowFullScreen
